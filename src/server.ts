860356b8729f8de62a71776557c01984d2858ee8
DhanushkaTa <tharindu@leadinnovationz.com>
2026-01-26 21:56:30 +0530

    connect to db

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express from 'express'
 import {PORT} from "./config/env";
+import connectToDatabase from "./db/database";
+import cookieParser from "cookie-parser";
 
 let app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cookieParser());
 
-
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is running on http://localhost:${PORT}`)
+
+    // connect to database
+    await connectToDatabase();
 })
