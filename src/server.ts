17ee33a5f7b4393b13e64981c2080dc8dc171879
DhanushkaTa <tharindu@leadinnovationz.com>
2026-01-26 21:22:21 +0530

    Initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello")
+import express from 'express'
+import {PORT} from "./config/env";
+
+let app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
